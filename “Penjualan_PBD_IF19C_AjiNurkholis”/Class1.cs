3b3f817f4b66e56252670cdb147b4439cfaf1888
Aji Nurkholis <ajinurkholis78@gmail.com>
2021-03-31 10:33:25 +0700

    asd

---
+++ “Penjualan_PBD_IF19C_AjiNurkholis”/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Penjualan_PBD_IF19C_AjiNurkholis_
+{
+    class Class1
+    {
+        static void main (string[] args)
+        {
+            Console.WriteLine("Hello word");
+        }
+    }
+}
